443e90c22706cfbada71907110031337451b610d
rajputshashank003 <hariomshashank@gmail.com>
2025-06-05 00:23:10 +0530

    feat: add scale on tap

--- src/components/Limited_Discount.tsx
+++ src/components/Limited_Discount.tsx
@@ -1,4 +1,5 @@
 import gsap from "gsap"
+import { motion } from "motion/react";
 import { useRef, useState } from "react";
 
 const Limited_Discount = () => {
@@ -110,11 +111,19 @@ const Limited_Discount = () => {
                     <div className="col-span-3 shadow-sm shadow-gray-400 h-[60px] rounded-[12px]  bg-red-600">
 
                     </div>
-                    <div onClick={handleClick} className="col-span-1 relative shadow-sm h-[60px] cursor-pointer shadow-gray-400 text-red-600 flex justify-center items-center rounded-[12px] bg-zinc-100 ">
+                    <motion.div 
+                        whileTap={{
+                            scale: 0.8
+                        }}
+                        transition={{
+                            duration: 0.4,
+                        }}
+                        onClick={handleClick} className="col-span-1 relative shadow-sm h-[60px] cursor-pointer shadow-gray-400 text-red-600 flex justify-center items-center rounded-[12px] bg-zinc-100 "
+                    >
                         <div ref={notification} className="absolute -top-[8px] -right-[8px] bg-red-600 h-[26px] w-[26px] rounded-full text-white font-bold flex justify-center items-center font-mono">1</div>
                         <svg ref={icon_discount}  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="currentColor"  className="icon icon-tabler absolute icons-tabler-filled icon-tabler-rosette-discount"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12.01 2.011c.852 0 1.668 .34 2.267 .942l.698 .698a1.2 1.2 0 0 0 .845 .349h1a3.2 3.2 0 0 1 3.2 3.2v1c0 .316 .126 .62 .347 .843l.698 .698a3.2 3.2 0 0 1 .002 4.536l-.698 .698a1.2 1.2 0 0 0 -.349 .845v1a3.2 3.2 0 0 1 -3.2 3.2h-1a1.2 1.2 0 0 0 -.843 .347l-.698 .698a3.2 3.2 0 0 1 -4.536 .002l-.698 -.698a1.2 1.2 0 0 0 -.845 -.349h-1a3.2 3.2 0 0 1 -3.2 -3.2v-1a1.2 1.2 0 0 0 -.347 -.843l-.698 -.698a3.2 3.2 0 0 1 -.002 -4.536l.698 -.698a1.2 1.2 0 0 0 .349 -.845v-1l.005 -.182a3.2 3.2 0 0 1 3.195 -3.018h1a1.2 1.2 0 0 0 .843 -.347l.698 -.698a3.2 3.2 0 0 1 2.269 -.944m2.49 10.989a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0 -3m1.207 -4.707a1 1 0 0 0 -1.414 0l-6 6a1 1 0 0 0 1.414 1.414l6 -6a1 1 0 0 0 0 -1.414m-6.207 -.293a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0 -3" /></svg>
                         <svg style={{ scale: 0 }} ref={icon_cross}  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler absolute icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
-                    </div>
+                    </motion.div>
                 </div>
                 <div className="grid grid-cols-4 gap-[10px]">
                     <div className="col-span-1 h-[60px] rounded-[12px]  bg-transparent">
